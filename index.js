fed7b464a7cf51abef6dc0b5333b8f94f02187c0
Gabriella Romero <rgabriella752@gmail.com>
2023-04-26 17:16:29 -0700

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = (["Guadalupe", "Ollie", "Aki"],"surprise")
+
+    function writeCards(name, value) {
+    let myArrray = []
+        for(let  loopCounter = 0; loopCounter < name.length; loopCounter++){
+        myArrray.push(`Thank you, ${name[loopCounter]}, for the wonderful surprise gift!`)}
+    return myArrray
+ }
+
+ function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+ }
+
+    
+  
